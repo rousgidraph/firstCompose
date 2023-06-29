15c27983722a7d9944c1fd85ce9cf21ec64cbb3c
rousgidraph <rousgidraph@gmail.com>
2023-06-29 13:21:00 +0300

    dealing with over flow

--- app/src/main/java/com/example/firstcomposeapplication/TextComposeTest.kt
+++ app/src/main/java/com/example/firstcomposeapplication/TextComposeTest.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -14,12 +15,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.firstcomposeapplication.ui.theme.FirstComposeApplicationTheme
 
 class TextComposeTest : ComponentActivity() {
@@ -33,7 +39,8 @@ class TextComposeTest : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     Column(modifier = Modifier.fillMaxSize()) {
-                        Text(text = stringResource(id = R.string.app_name),
+                        Text(
+                            text = stringResource(id = R.string.app_name),
                             modifier = Modifier
                                 .background(MaterialTheme.colors.primary)
                                 .padding(16.dp)
@@ -46,32 +53,56 @@ class TextComposeTest : ComponentActivity() {
 }
 
 @Composable
-fun customText2(){
-    Text(buildAnnotatedString {
-        append("A")
-        append("B")
-        append("C")
-        append("D")
-        append("E")
-    })
+fun customText2() {
+    Text(
+        buildAnnotatedString {
+            withStyle(style = ParagraphStyle(textAlign = TextAlign.Center)) {
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Red,
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                ) {
+                    append("A")
+                }
+                append("B")
+                append("C")
+                append("D")
+                append("E")
+            }
+        }, modifier = Modifier.width(200.dp)
+    )
 }
 
+
+
+@Composable
+fun customText3(){
+    Text(text = "Hello world".repeat(20), maxLines = 2, overflow = TextOverflow.Ellipsis)
+}
+
+@Composable
+fun customText(){
+    Text(
+        text = stringResource(id = R.string.app_name),
+        modifier = Modifier
+            .background(MaterialTheme.colors.primary)
+            .padding(16.dp),
+        color = Color.White,
+        fontSize = MaterialTheme.typography.h6.fontSize,
+        fontStyle = FontStyle.Italic,
+        fontWeight = FontWeight.ExtraBold,
+        textAlign = TextAlign.End
+
+    )
+}
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview2() {
     FirstComposeApplicationTheme {
-       Column(modifier = Modifier.fillMaxSize()) {
-           Text(text = stringResource(id = R.string.app_name),
-               modifier = Modifier
-                   .background(MaterialTheme.colors.primary)
-                   .padding(16.dp),
-               color= Color.White,
-               fontSize = MaterialTheme.typography.h6.fontSize,
-               fontStyle = FontStyle.Italic,
-               fontWeight = FontWeight.ExtraBold,
-               textAlign = TextAlign.End
-
-           )
-       }
+        Column(modifier = Modifier.fillMaxSize()) {
+            customText3()
+        }
     }
 }
